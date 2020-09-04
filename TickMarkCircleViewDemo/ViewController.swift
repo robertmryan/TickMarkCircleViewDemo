ab4b558edfe3df79a287f505bea2bf3234bc0848
Robert Ryan <robert.ryan@mindspring.com>
2020-09-04 09:48:55 -0700

    Initial Commit

---
+++ TickMarkCircleViewDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TickMarkCircleViewDemo
+//
+//  Created by Robert Ryan on 9/4/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
